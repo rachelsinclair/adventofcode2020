21c34d5173b674859ad96e14b6f085f56283c500
Rachel Sinclair <12307504+rachelsinclair@users.noreply.github.com>
2020-12-10 23:41:38 +0000

    minor refactor in day 4 code

--- day4/day4.py
+++ day4/day4.py
@@ -4,6 +4,10 @@ required_fields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 with open('input.txt') as input_file:
     passports = [passport.replace("\n"," ") for passport in input_file.read().split("\n\n")]
 
-count_of_valid_passports = sum(all(field in passport for field in required_fields) for passport in passports)
+def is_valid_passport(passport):
+    return all(field in passport for field in required_fields)
+
+
+count_of_valid_passports = sum(is_valid_passport(passport) for passport in passports)
 print(count_of_valid_passports)
 # %%
