cd9df94fee547f5f8a929ac89e061531d05f94f8
Rachel Sinclair <12307504+rachelsinclair@users.noreply.github.com>
2020-12-17 22:50:55 +0000

    refactor day 8 part 1 and begin part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,19 +1,11 @@
-with open('input.txt') as input_file:
-    instructions = [line.rstrip().split(" ") for line in input_file.readlines()]
+def get_instructions():
+    with open('input.txt') as input_file:
+        instructions = [line.rstrip().split(" ") for line in input_file.readlines()]
+    return instructions
 
-print(instructions[:5])
-
-pointer = 0
-accumulator = 0
-program_is_running = True
-
-def run_instruction():
-    global pointer, instructions, accumulator, program_is_running
-    current_instruction = instructions[pointer]
-    operation = current_instruction[0]
-    current_instruction[0] = "xxx"
-    argument = int(current_instruction[1])
-    print(f"Running line {pointer}: {operation} {argument}")
+def run_instruction(instruction, pointer, accumulator, program_is_running):
+    operation = instruction[0]
+    argument = int(instruction[1])
     if (operation == 'nop'):
         pass
     elif (operation == 'acc'):
@@ -22,9 +14,27 @@ def run_instruction():
         pointer = pointer + argument - 1
     elif (operation == 'xxx'):
         program_is_running = False
-    
+    return (pointer, accumulator, program_is_running)
 
-while (program_is_running):
-    run_instruction()
-    pointer = pointer + 1
-print(f"loop broken, acc = {accumulator}")
+def part_1():
+    instructions = get_instructions()
+    pointer = 0
+    accumulator = 0
+    program_is_running = True
+    while (program_is_running):
+        current_instruction: list = instructions[pointer]
+        (pointer, accumulator, program_is_running) = run_instruction(current_instruction, pointer, accumulator, program_is_running)
+        current_instruction[0] = 'xxx'
+        pointer = pointer + 1
+    print(f"loop broken, acc = {accumulator}")
+
+def part_2():
+    instructions = get_instructions()
+    inst_length = len(instructions)
+    print(inst_length)
+    for index, instruction in instructions:
+        if instruction[0] == "nop" and index + int(instruction[1]) == inst_length:
+            print(index, instruction)
+            
+part_1()
+part_2()
