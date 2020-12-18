25543e55f53029facbcbf3acf4c57c50414b3ef8
Rachel Sinclair <12307504+rachelsinclair@users.noreply.github.com>
2020-12-18 00:50:23 +0000

    solve day 8 part 2

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,8 @@
+import copy
+
+class InfiniteLoopException(BaseException):
+    pass
+
 def get_instructions():
     with open('input.txt') as input_file:
         instructions = [line.rstrip().split(" ") for line in input_file.readlines()]
@@ -14,27 +19,61 @@ def run_instruction(instruction, pointer, accumulator, program_is_running):
         pointer = pointer + argument - 1
     elif (operation == 'xxx'):
         program_is_running = False
+        raise InfiniteLoopException()
     return (pointer, accumulator, program_is_running)
 
-def part_1():
-    instructions = get_instructions()
+def run_program(instruction_list):
+    instructions = copy.deepcopy(instruction_list)
     pointer = 0
     accumulator = 0
     program_is_running = True
+    executed_instructions = []
     while (program_is_running):
-        current_instruction: list = instructions[pointer]
-        (pointer, accumulator, program_is_running) = run_instruction(current_instruction, pointer, accumulator, program_is_running)
-        current_instruction[0] = 'xxx'
-        pointer = pointer + 1
-    print(f"loop broken, acc = {accumulator}")
+        try:
+            current_instruction = instructions[pointer]
+            executed_instructions.append((pointer, current_instruction.copy()))
+            (pointer, accumulator, program_is_running) = run_instruction(current_instruction, pointer, accumulator, program_is_running)
+            current_instruction[0] = 'xxx'
+            pointer = pointer + 1
+        except IndexError as error:
+            raise IndexError(f"Escaped boot code, acc value: {accumulator}")
+        except InfiniteLoopException as error:
+            print(f"Infinite boot loop, acc value: {accumulator}")
+            break
+    return executed_instructions
+
+
+def part_1():
+    instructions = get_instructions()
+    try:
+        run_program(instructions)
+    except InfiniteLoopException as error:
+        pass
 
 def part_2():
     instructions = get_instructions()
-    inst_length = len(instructions)
-    print(inst_length)
-    for index, instruction in instructions:
-        if instruction[0] == "nop" and index + int(instruction[1]) == inst_length:
-            print(index, instruction)
-            
+    relevant_instructions = run_program(instructions)
+    for index, [operator, argument] in relevant_instructions:
+        if operator == "jmp":
+            modified_instructions = copy.deepcopy(instructions)
+            modified_instructions[index][0] = "nop"
+            try:
+                run_program(modified_instructions)
+            except IndexError as error:
+                print(error)
+                break
+            except InfiniteLoopException as error:
+                pass
+        elif operator == "nop":
+            modified_instructions = copy.deepcopy(instructions)
+            modified_instructions[index][0] = "jmp"
+            try:
+                run_program(modified_instructions)
+            except IndexError as error:
+                print(error)
+                break
+            except InfiniteLoopException as error:
+                pass
+
 part_1()
 part_2()
